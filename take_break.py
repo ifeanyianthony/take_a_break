a0ae8827edfa4e5d03c64719b06a031b397a9a86
Ifeanyi Anthony <ifeanyianthony@gmail.com>
2021-05-12 13:50:56 +0100

    loop: added loop to launch the browser 3X before stopping

--- take_break.py
+++ take_break.py
@@ -1,5 +1,12 @@
 import webbrowser
 import time
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=5qhEJIgnC08&list=RD5qhEJIgnC08&start_radio=1")
+total_breaks = 3
+break_count = 0
+
+## Opens the link after waiting for 10 seconds.
+print('This program started on '+time.ctime())
+while (break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=5qhEJIgnC08&list=RD5qhEJIgnC08&start_radio=1")
+    break_count +=1
